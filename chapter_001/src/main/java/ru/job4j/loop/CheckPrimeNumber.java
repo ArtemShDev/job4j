a21e32dd7601e1e89862713c31ad411c0555a973
ArtemShDev <dev.art.balt@gmail.com>
2020-01-10 21:37:34 +0300

    5.6. Простые числа.[#212760]

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,9 +2,16 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int finish) {
-        boolean prime = true;
-        for (int y = 1; y <= finish; y++) {
-            if(finish==1 || ((y!=finish) && y!=1) && (finish % y) == 0) {
+        //boolean prime = true;
+        //for (int y = 1; y <= finish; y++) {
+        //    if(finish==1 || ((y!=finish) && y!=1) && (finish % y) == 0) {
+        //        prime = false;
+//                break;
+//            }
+//        }
+        boolean prime = finish > 1;
+        for (int y = 2; y < finish; y++) {
+            if (finish % y == 0) {
                 prime = false;
                 break;
             }
