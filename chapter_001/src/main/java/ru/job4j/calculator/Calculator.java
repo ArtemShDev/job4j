e58042b8d0b2f76317b67ddc100cda6b23a8d261
ArtemShDev <dev.art.balt@gmail.com>
2020-01-11 21:56:27 +0300

    5.8. JavaDoc. Code convention[#212767]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,5 +1,12 @@
 package ru.job4j.calculator;
 
+/**
+*   Class – Класс для вычисления арифмитических операций;
+*   @author – Shmelev Artem dev.art.balt@gmail.com;
+*   @version – 1.0
+*   @since - 11.01.2020
+*/
+
 public class Calculator {
 
     public static void add(double first, double second) {
@@ -22,6 +29,11 @@ public class Calculator {
         System.out.println(first + "-" + second + " = " + result);
     }
 
+    /**
+     *   Method main
+     *   Производит арифметические операции "+ - / *"
+     *   @param args - args
+     */
     public static void main(String[] args) {
         add(1, 1);
         div(4, 2);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Shmelev Artem (dev.art.balt@gmail.com)
+ * @version 1.0
+ * @since 11.01.2020
+ */
+package ru.job4j.calculator;
